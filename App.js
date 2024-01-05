18f57313bdccd93757925dc506d69c6c4e163557
akhil-lakka <akhillakka@gmail.com>
2024-01-05 09:12:27 +0530

    func components

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: " 123 " }, "I'm h1 tag"),
-    React.createElement("h2", { key: " 124 " }, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { key: " 123 " }, "I'm h1 tag"),
-    React.createElement("h2", { key: " 124 " }, "I'm h2 tag"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
+const elem = <span>React element </span>;
+
+// React Element
+const Title = () => (
+  <h1 className="head" tabIndex="1">
+    React using JSX 🚀
+  </h1>
 );
 
-console.log(parent);
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Hello World Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
